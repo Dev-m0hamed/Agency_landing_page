b93ca5bf0112d79aa893bf0eb829118a8c925739
Mohamed <010232066mo@gmail.com>
2025-09-26 20:02:25 +0300

    ''

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Agency_landing_page/",
   plugins: [tailwindcss()],
 });
